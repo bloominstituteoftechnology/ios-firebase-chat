73b7d09b08c6eac7c1a08911c21f330e21f66243
Lisa Sampson <lisasampson0918@gmail.com>
2018-09-18 17:05:20 -0400

    Set up skeleton of app

---
+++ Firebase Chat/Firebase Chat/Model Controllers/FirebaseChatController.swift
@@ -0,0 +1,9 @@
+//
+//  FirebaseChatController.swift
+//  Firebase Chat
+//
+//  Created by Lisa Sampson on 9/18/18.
+//  Copyright © 2018 Lisa Sampson. All rights reserved.
+//
+
+import Foundation
