9e4f47772b4b7a512a03d9a6d3d6db2b859c822c
Lisa Sampson <lisasampson0918@gmail.com>
2018-09-18 16:53:54 -0400

    add frameworks

---
+++ Firebase Chat/Firebase Chat/View Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Firebase Chat
+//
+//  Created by Lisa Sampson on 9/18/18.
+//  Copyright © 2018 Lisa Sampson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
