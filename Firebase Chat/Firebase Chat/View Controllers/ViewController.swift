a4c56d38ba5a8d8fe01625ba02212b1b6122d047
Linh Bouniol <paulalinh89@gmail.com>
2018-09-18 13:26:11 -0700

    Created xcode project

---
+++ Firebase Chat/Firebase Chat/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Firebase Chat
+//
+//  Created by Linh Bouniol on 9/18/18.
+//  Copyright © 2018 Linh Bouniol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
