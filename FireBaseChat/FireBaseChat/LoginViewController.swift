e1872147daa86f9eebe7c5bf80f19d2d1ba320d1
Carolyn Lea <carolynrlea@gmail.com>
2018-09-18 20:38:08 -0600

    made changes

---
+++ FireBaseChat/FireBaseChat/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  FirebaseChat
+//
+//  Created by Carolyn Lea on 9/18/18.
+//  Copyright © 2018 Carolyn Lea. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FireBaseChat/FireBaseChat/Notifications.swift
@@ -0,0 +1,9 @@
+//
+//  Notifications.swift
+//  FirebaseChat
+//
+//  Created by Carolyn Lea on 9/18/18.
+//  Copyright © 2018 Carolyn Lea. All rights reserved.
+//
+
+import Foundation
