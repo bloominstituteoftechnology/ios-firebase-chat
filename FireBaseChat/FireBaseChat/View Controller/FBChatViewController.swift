46dabaf31d3e55e9426fe636481eed486b8363e4
Jerrick Warren <Jerrick.Warren@gmail.com>
2019-01-09 07:19:58 -0800

    storyboard setup

---
+++ FireBaseChat/FireBaseChat/View Controller/FBChatViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FBChatViewController.swift
+//  FireBaseChat
+//
+//  Created by Jerrick Warren on 1/9/19.
+//  Copyright © 2019 Jerrick Warren. All rights reserved.
+//
+
+import UIKit
+
+class FBChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
