2e28afca1ea336eff2f889c284a77216cb1c99ed
Ben Hakes <bhhakes@gmail.com>
2019-02-12 15:30:22 -0600

    add project template file

---
+++ ios-firebase-chat/ios-firebase-chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-firebase-chat
+//
+//  Created by Benjamin Hakes on 2/12/19.
+//  Copyright © 2019 Benjamin Hakes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
