ec9c8d7daec7a9dc3f4e8795d28a009552fd1981
jkaunert <44586402+jkaunert@users.noreply.github.com>
2019-02-13 03:23:48 -0600

    (feat) implements ChatRoomListTVC and ChatroomVC conforming to MessageKit
    Also implements mostly complete UI in Interface Builder.

--- ios-firebase-chat/ios-firebase-chat/View Controllers/ChatRoomListTableViewController.swift
+++ ios-firebase-chat/ios-firebase-chat/View Controllers/ChatRoomListTableViewController.swift
@@ -5,7 +5,7 @@ import Foundation
 import FirebaseDatabase
 
 class ChatRoomListTableViewController: UITableViewController {
-
+    let reuseIdentifier = "ChatroomCell"
     let chatRoomController = ChatRoomController()
     
     override func viewWillAppear(_ animated: Bool) {
@@ -21,7 +21,7 @@ class ChatRoomListTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let userNameAlert = UIAlertController(title: "Please enter your username:", message: nil, preferredStyle: .actionSheet)
+        let userNameAlert = UIAlertController(title: "Please enter your username:", message: nil, preferredStyle: .alert)
         var userNameTextField: UITextField?
         userNameAlert.addTextField { (textField) in
             textField.placeholder = "Username:"
@@ -42,68 +42,46 @@ class ChatRoomListTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return chatRoomController.chatRooms.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
         // Configure the cell...
-
+        cell.textLabel?.text = chatRoomController.chatRooms[indexPath.row].title
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+ 
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
+        guard let indexPath = tableView.indexPathForSelectedRow else { return }
+        guard let destinationVC = segue.destination as? ChatRoomViewController else { return }
         // Pass the selected object to the new view controller.
+        destinationVC.chatRoomController = chatRoomController
+        destinationVC.chatRoom = chatRoomController.chatRooms[indexPath.row]
+        
     }
-    */
+ 
+    // MARK: - Properties(@IBOutlets)
+    @IBOutlet weak var chatRoomTitleTextField: UITextField!
 
+    // MARK: - @IBActions
+    
+    @IBAction func createNewChatRoom(_ sender: Any) {
+        chatRoomTitleTextField.resignFirstResponder()
+        guard let chatRoomTitle = chatRoomTitleTextField.text else { return }
+        chatRoomTitleTextField.text = ""
+        chatRoomController.createChatRoom(with: chatRoomTitle) {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
 }
