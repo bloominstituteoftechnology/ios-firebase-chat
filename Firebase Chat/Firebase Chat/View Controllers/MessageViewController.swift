e104caaadd4db2977d7e5c2f9af63f3e0c76c0c4
Audrey Welch <audreywelch@Audreys-MacBook-Pro.local>
2019-02-12 20:02:21 -0600

    Completed protocol stubs for detail view

--- Firebase Chat/Firebase Chat/View Controllers/MessageViewController.swift
+++ Firebase Chat/Firebase Chat/View Controllers/MessageViewController.swift
@@ -1,9 +1,52 @@
 
 import UIKit
+import MessageKit
+import MessageInputBar
 
-class MessageViewController: UIViewController {
-    
+class MessageViewController: MessagesViewController, MessagesDataSource, MessagesDisplayDelegate, MessagesLayoutDelegate, MessageInputBarDelegate {
+
     var chatRoom: ChatRoom?
     var modelController: ModelController?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        messageInputBar.delegate = self
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
+    }
+    
+    func currentSender() -> Sender {
+        guard let sender = modelController?.currentUser else {
+            return Sender(id: "", displayName: "User")
+        }
+        return sender
+    }
+    
+    func isFromCurrentSender(message: MessageType) -> Bool {
+        return message.sender == currentSender()
+    }
+    
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        guard let message = chatRoom?.messages[indexPath.row] else {
+            fatalError("Messages not available")
+        }
+        return message
+    }
+    
+    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+        return 1
+    }
+    
+    func numberOfItems(inSection section: Int, in messagesCollectionView: MessagesCollectionView) -> Int {
+        return chatRoom?.messages.count ?? 0
+    }
+    
+    func messageTopLabelAttributedText(for message: MessageType, at indexPath: IndexPath) -> NSAttributedString? {
+        let name = message.sender.displayName
+        let attrs = [NSAttributedString.Key.font : UIFont.preferredFont(forTextStyle: .caption1)]
+        return NSAttributedString(string: name, attributes: attrs)
+    }
     
 }
