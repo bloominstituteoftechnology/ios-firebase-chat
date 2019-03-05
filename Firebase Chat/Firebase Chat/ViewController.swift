50c999ea2a6e1cc8919dbe9d44aa055317aebdd7
Moses Robinson <mosesjrobinson@icloud.com>
2019-03-05 14:57:28 -0800

    initial commit

---
+++ Firebase Chat/Firebase Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Firebase Chat
+//
+//  Created by Moses Robinson on 3/5/19.
+//  Copyright © 2019 Moses Robinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
