997705b0ad7708d74e0b5890b7c42784c7f65a89
Angel <angelbuenrostro@outlook.com>
2019-03-13 05:52:02 -0400

    Add firebase and message kit

---
+++ Firebase Chat/Firebase Chat/Views/FirebaseChatDetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  FirebaseChatDetailViewController.swift
+//  Firebase Chat
+//
+//  Created by Angel Buenrostro on 3/13/19.
+//  Copyright © 2019 Angel Buenrostro. All rights reserved.
+//
+
+import UIKit
+import MessageKit
+import MessageInputBar
+
+class FirebaseChatDetailViewController: MessagesViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
