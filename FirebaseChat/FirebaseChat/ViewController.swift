fa1f5adf56c1fb4f5c4d53ea1ca10d6fc59f9cba
Christopher Aronson <christopheraronson@icloud.com>
2019-06-18 11:48:28 -0700

    Created Xcode Project

---
+++ FirebaseChat/FirebaseChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseChat
+//
+//  Created by Christopher Aronson on 6/18/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
