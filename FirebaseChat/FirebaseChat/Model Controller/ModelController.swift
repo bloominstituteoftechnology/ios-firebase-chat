7c46107fad8d14d08db954cefe9fefd89553cdc9
Christopher Aronson <christopheraronson@icloud.com>
2019-06-18 18:58:12 -0700

    Built skeleton for createMessage and FetchMessages.

--- FirebaseChat/FirebaseChat/Model Controller/ModelController.swift
+++ FirebaseChat/FirebaseChat/Model Controller/ModelController.swift
@@ -15,6 +15,7 @@ class ModelController {
     lazy var db = Firestore.firestore()
     
     var chatRooms = [ChatRoom]()
+    var messages = [Message]()
     
     func createChatRoom(with chatRoomName: String, completion: @escaping (Error?) -> Void) {
         
@@ -53,7 +54,44 @@ class ModelController {
         }
     }
     
+    func createMessage(in chatRoom: ChatRoom, with message: Message, completion: @escaping (Error?) -> Void) {
+        
+        db.collection("chatRooms")
+            .document(chatRoom.chatRoomName)
+            .collection("messages")
+            .addDocument(data: [
+                "sender" : message.sender,
+                "timestamp" : message.timestamp,
+                "message" : message.message
+            ]) { error in
+                
+                if let error = error {
+                    completion(error)
+                    return
+                }
+                
+                completion(nil)
+        }
+    }
     
+    func fetchMessages(in chatRoom: ChatRoom, completion: @escaping (Error?) -> Void) {
+        
+        db.collection("chatRooms").document(chatRoom.chatRoomName).collection("messages").getDocuments { (snapshot, error) in
+            
+            if let error = error {
+                completion(error)
+                return
+            }
+            
+            guard let documents = snapshot?.documents else { return }
+            
+            for document in documents {
+                
+                print(document.data())
+                
+            }
+        }
+    }
     
 }
 
