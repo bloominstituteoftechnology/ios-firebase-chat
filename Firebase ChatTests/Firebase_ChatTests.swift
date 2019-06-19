b8870bee2442eacb466b7ff3e0ca4b5bc6ea23c3
Michael Redig <mredig@gmail.com>
2019-06-19 00:08:54 -0500

    updated tests for new message format

--- Firebase ChatTests/Firebase_ChatTests.swift
+++ Firebase ChatTests/Firebase_ChatTests.swift
@@ -12,43 +12,48 @@ import Firebase
 
 class Firebase_ChatTests: XCTestCase {
 
-//	func testConvertMessageToDictionary() {
-//		let inMessage = "This is a message"
-//		let sender = "ME"
-//		let date = Date()
-//		let id = UUID()
-//		let message = Message(text: inMessage, timestamp: date, sender: sender, senderID: id)
-//
-//		let dict = try! message.toDict()
-//
-//		XCTAssertNotNil(dict)
-//		let testText = dict["text"] as! String
-//		let testTime = dict["timestamp"] as! Double
-//		let testSender = dict["sender"] as! String
-//		let testSenderID = dict["senderID"] as! String
-//		XCTAssertEqual(testText, inMessage)
-//		XCTAssertEqual(testTime, date.timeIntervalSince1970)
-//		XCTAssertEqual(testSender, sender)
-//		XCTAssertEqual(testSenderID, id.uuidString)
-//
-//	}
-//
-//	func testConvertMessageFromDictionary() {
-//		let inMessage = "This is a message"
-//		let sender = "ME"
-//		let date = Date()
-//		let id = UUID()
-//
-//		let dict = ["text": inMessage, "timestamp": date.timeIntervalSince1970, "sender": sender, "senderID": id.uuidString] as [String : Any]
-//
-//		let message = try! Message.fromDict(dict)
-//
-//		XCTAssertNotNil(message)
-//		XCTAssertEqual(inMessage, message.text)
-//		XCTAssertEqual(sender, message.sender)
-//		XCTAssertEqual(date.timeIntervalSince1970, message.timestamp.timeIntervalSince1970)
-//		XCTAssertEqual(id, message.senderID)
-//	}
+	func testConvertMessageToDictionary() {
+		let text = "This is a message"
+		let senderName = "ME"
+		let sentDate = Date()
+		let id = UUID()
+		let senderID = UUID()
+		let message = Message(text: text, senderName: senderName, senderID: senderID, id: id, sentDate: sentDate)
+
+		let dict = try! message.toDict()
+
+		XCTAssertNotNil(dict)
+		let testText = dict["text"] as! String
+		let testTime = dict["sentDate"] as! Double
+		let testSender = dict["senderName"] as! String
+		let testSenderID = dict["senderID"] as! String
+		let testID = dict["id"] as! String
+		XCTAssertEqual(testText, text)
+		XCTAssertEqual(testTime, sentDate.timeIntervalSince1970)
+		XCTAssertEqual(testSender, senderName)
+		XCTAssertEqual(testSenderID, senderID.uuidString)
+		XCTAssertEqual(testID, id.uuidString)
+
+	}
+
+	func testConvertMessageFromDictionary() {
+		let text = "This is a message"
+		let senderName = "ME"
+		let sentDate = Date()
+		let id = UUID()
+		let senderID = UUID()
+
+		let dict = ["text": text, "sentDate": sentDate.timeIntervalSince1970, "senderName": senderName, "senderID": senderID.uuidString, "id": id.uuidString] as [String : Any]
+
+		let message = try! Message.fromDict(dict)
+
+		XCTAssertNotNil(message)
+		XCTAssertEqual(text, message.text)
+		XCTAssertEqual(senderName, message.senderName)
+		XCTAssertEqual(sentDate.timeIntervalSince1970, message.sentDate.timeIntervalSince1970)
+		XCTAssertEqual(senderID, message.senderID)
+		XCTAssertEqual(id, message.id)
+	}
 
 	func testConvertChatroomToDictionary() {
 		let topic = "I'm Mr. Meeseeks look at me!"
