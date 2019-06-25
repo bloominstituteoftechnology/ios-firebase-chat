250e4a2de35286a47bd54ad873dac4b23799828d
hectorsvill <hectorstevenvillasano@gmail.com>
2019-06-25 12:33:09 -0700

    init

---
+++ ios-firebase-chat/ios-firebase-chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-firebase-chat
+//
+//  Created by Hector Steven on 6/25/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
