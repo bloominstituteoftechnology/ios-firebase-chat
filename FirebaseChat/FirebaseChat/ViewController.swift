7b187c6e1844cf0a8296b1577206e9566a658b0c
Sameera Leola <pixelandbyte@gmail.com>
2019-06-25 17:57:57 -0500

    Initial commit

---
+++ FirebaseChat/FirebaseChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseChat
+//
+//  Created by Sameera Roussi on 6/25/19.
+//  Copyright © 2019 Sameera Roussi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
