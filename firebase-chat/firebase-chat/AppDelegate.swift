135941f7e7be3735f9318fce4a636495cd3f8c74
hectorsvill <hectorstevenvillasano@gmail.com>
2019-06-26 00:08:02 -0700

    ref.child("rooms")

--- firebase-chat/firebase-chat/AppDelegate.swift
+++ firebase-chat/firebase-chat/AppDelegate.swift
@@ -18,7 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		FirebaseApp.configure()
-		RoomsController().test()
+//		RoomsController().testpush()
+		RoomsController().test2get()
 		return true
 	}
 
--- firebase-chat/firebase-chat/Model Controller/RoomsController.swift
+++ firebase-chat/firebase-chat/Model Controller/RoomsController.swift
@@ -11,12 +11,23 @@ import Firebase
 
 class RoomsController {
 
-	func test() {
-		ref.child("rooms")
+	func testpush() {
+		//self.ref.child("users").child(user.uid).setValue(["username": username])
+		let room1 = Room(roomname: "Room#2")
+		
+		ref.child("rooms").child(room1.id).setValue(["name": room1.roomname])
 	}
 	
-	func test2() {
-		
+	func test2get() {
+		ref.child("rooms").observe(DataEventType.value, with: { (snapshot) in
+			if let postDict = snapshot.value as? [String: [String: String]] {
+				let rooms = Array(postDict.values)
+				print(rooms[0]["name"]!)
+				
+			} else {
+				fatalError()
+			}
+		})
 	}
 	
 	init(ref: DatabaseReference = Database.database().reference()) {
