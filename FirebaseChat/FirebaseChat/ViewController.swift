faa977ee8bec751ccc8dfc76adbaccb9f0a4536a
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-07-02 15:42:51 -0400

    Added Firebase and MessageKit Frameworks with CocoaPods

---
+++ FirebaseChat/FirebaseChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseChat
+//
+//  Created by Thomas Cacciatore on 7/2/19.
+//  Copyright © 2019 Thomas Cacciatore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
