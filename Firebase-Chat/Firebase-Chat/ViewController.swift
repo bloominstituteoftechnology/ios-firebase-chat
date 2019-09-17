67ed1a4a466591c34a33ca1dc3e550d5db1a3794
Marlon <marmoneymar@icloud.com>
2019-09-17 18:33:41 -0400

    Initial Commit

---
+++ Firebase-Chat/Firebase-Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Firebase-Chat
+//
+//  Created by Marlon Raskin on 9/17/19.
+//  Copyright © 2019 Marlon Raskin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
