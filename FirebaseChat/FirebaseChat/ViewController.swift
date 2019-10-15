9d4f0000011ab9750964e7d4e57de012e9d4e3d0
Jake <jlconnerly@gmail.com>
2019-10-15 15:18:03 -0500

    Setup firebase complete

---
+++ FirebaseChat/FirebaseChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseChat
+//
+//  Created by Jake Connerly on 10/15/19.
+//  Copyright © 2019 jake connerly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
