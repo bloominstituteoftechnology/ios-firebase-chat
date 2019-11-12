d5101c663986026708daff48b0a6d2e4779fdeed
Jerry Haaser <jerry.haaser@hotmail.com>
2019-11-12 16:23:17 -0600

    set up UI

---
+++ FirebaseChatApp/Pods/Target Support Files/Pods-FirebaseChatApp-FirebaseChatAppUITests/Pods-FirebaseChatApp-FirebaseChatAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FirebaseChatApp_FirebaseChatAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FirebaseChatApp_FirebaseChatAppUITests
+@end
---
+++ FirebaseChatApp/Pods/Target Support Files/Pods-FirebaseChatAppTests/Pods-FirebaseChatAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FirebaseChatAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_FirebaseChatAppTests
+@end
