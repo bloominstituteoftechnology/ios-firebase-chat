2d5c51371aed6d00f38e09fa4627fb4866a83d1e
Jesse Ruiz <jesseleeruiz@gmail.com>
2019-11-12 09:53:39 -1000

    Initial Commit. Added xcode project.

---
+++ firebaseChats/firebaseChats/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firebaseChats
+//
+//  Created by Jesse Ruiz on 11/12/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
