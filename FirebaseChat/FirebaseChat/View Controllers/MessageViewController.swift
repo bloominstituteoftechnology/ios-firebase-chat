d99ff178cbc4fc99dab2e5107f945be48bd57a85
Gi Pyo Kim <gipyok@gmail.com>
2019-11-12 14:26:55 -0800

    Basic UI setup

---
+++ FirebaseChat/FirebaseChat/View Controllers/MessageViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MessageViewController.swift
+//  FirebaseChat
+//
+//  Created by Gi Pyo Kim on 11/12/19.
+//  Copyright © 2019 GIPGIP Studio. All rights reserved.
+//
+
+import UIKit
+import MessageKit
+
+class MessageViewController: MessagesViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
