53be2ccdb34796f060ded5cf86c95b85d8960503
Isvvc <lyons@tuta.io>
2019-11-12 14:38:55 -0700

    Set up storyboard

---
+++ Firebase Chat/Firebase Chat/ThreadViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ThreadViewController.swift
+//  Firebase Chat
+//
+//  Created by Isaac Lyons on 11/12/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+import MessageKit
+
+class ThreadViewController: MessagesViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
