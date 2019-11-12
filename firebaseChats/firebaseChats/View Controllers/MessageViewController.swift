f9f9e012f1183b47337b5ba9726886ffaa9fe905
Jesse Ruiz <jesseleeruiz@gmail.com>
2019-11-12 10:58:18 -1000

    Finished Storyboard Setup.

---
+++ firebaseChats/firebaseChats/View Controllers/MessageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MessageViewController.swift
+//  firebaseChats
+//
+//  Created by Jesse Ruiz on 11/12/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MessageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
