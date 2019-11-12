abbc4e09cf53e9049b459d111d7e8ba69a51dbb6
Wilma <kima_92@fastmail.com>
2019-11-12 18:52:30 -0500

    Implemented most of the Message Model.

--- Firebase Chat/Models/Message.swift
+++ Firebase Chat/Models/Message.swift
@@ -9,7 +9,65 @@
 import Foundation
 import MessageKit
 
-struct Message {
+struct Message: MessageType {
+    
+    //MARK: Properties
+    var messageId: String
+    var sentDate: Date
+    var text: String
+    var displayName: String
+    var senderId: String
+    
+    var kind: MessageKind { return .text(text)}
+    var sender: SenderType {
+        return Sender(senderId: senderId, displayName: displayName)
+    }
+    
+    init(sender: Sender, text: String, sentDate: Date = Date(), messageId: String = UUID().uuidString) {
+        self.text = text
+        self.displayName = sender.displayName
+        self.senderId = sender.senderId
+        self.sentDate = sentDate
+        self.messageId = messageId
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: MessageCodingKeys.self)
+
+        let text = try container.decode(String.self, forKey: .text)
+        let displayName = try container.decode(String.self, forKey: .displayName)
+        let senderId = try container.decode(String.self, forKey: .senderId)
+        let sentDate = try container.decode(Date.self, forKey: .sentDate)
+
+        let sender = Sender(senderId: senderId, displayName: displayName)
+
+        self.init(sender: sender, text: text, sentDate: sentDate)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: MessageCodingKeys.self)
+
+        try container.encode(displayName, forKey: .displayName)
+        try container.encode(senderId, forKey: .senderId)
+        try container.encode(sentDate, forKey: .sentDate)
+        try container.encode(text, forKey: .text)
+    }
+    
+    enum MessageCodingKeys: String, CodingKey {
+        case text
+        case messageId
+        case sentDate
+        case kind = "kind"
+        case displayName
+        case senderId
+        case sender = "sender"
+    }
     
 
+}
+
+struct Sender: SenderType {
+    
+    var senderId: String
+    var displayName: String
 }
