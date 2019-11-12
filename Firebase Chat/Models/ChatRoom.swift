434ce6842c247ff2f4d9939cbdb2e0846b784259
Wilma <kima_92@fastmail.com>
2019-11-12 17:55:25 -0500

    Added Message and ChatRoom models, plus ChatController.

---
+++ Firebase Chat/Models/ChatRoom.swift
@@ -0,0 +1,14 @@
+//
+//  ChatRoom.swift
+//  Firebase Chat
+//
+//  Created by macbook on 11/12/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import MessageKit
+
+struct ChatRoom {
+    
+}
---
+++ Firebase Chat/Models/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Firebase Chat
+//
+//  Created by macbook on 11/12/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import MessageKit
+
+struct Message {
+    
+
+}
