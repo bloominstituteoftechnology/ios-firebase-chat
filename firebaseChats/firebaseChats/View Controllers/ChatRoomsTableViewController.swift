10b4b8df91f89b858fcf10bd2c764f0228b63934
Jesse Ruiz <jesseleeruiz@gmail.com>
2019-11-12 17:59:52 -1000

    MVP. Made app respond like guided project.

--- firebaseChats/firebaseChats/View Controllers/ChatRoomsTableViewController.swift
+++ firebaseChats/firebaseChats/View Controllers/ChatRoomsTableViewController.swift
@@ -7,84 +7,107 @@
 //
 
 import UIKit
+import Firebase
 
 class ChatRoomsTableViewController: UITableViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var chatTextField: UITextField!
+    
+    // MARK: - Properties
+    
+    let chatRoomsController = ChatRoomsController()
+    
+    var ref: DatabaseReference! = Database.database().reference()
 
+    // MARK: - LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        if let currentUserDictionary = UserDefaults.standard.value(forKey: "currentUser") as? [String : String] {
+            let currentUser = Sender(dictionary: currentUserDictionary)
+            chatRoomsController.currentUser = currentUser
+        } else {
+            
+            let alert = UIAlertController(title: "Set a username", message: nil, preferredStyle: .alert)
+            
+            var usernameTextField: UITextField!
+            
+            alert.addTextField { (textField) in
+                textField.placeholder = "Username:"
+                usernameTextField = textField
+            }
+            
+            let submitAction = UIAlertAction(title: "Submit", style: .default) { (_) in
+                
+                let displayName = usernameTextField.text ?? "Unknown User"
+                let id = UUID().uuidString
+                
+                let sender = Sender(senderId: id, displayName: displayName)
+                
+                UserDefaults.standard.set(sender.dictionaryRepresentation, forKey: "currentUser")
+                
+                self.chatRoomsController.currentUser = sender
+            }
+            
+            alert.addAction(submitAction)
+            present(alert, animated: true, completion: nil)
+        }
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        chatRoomsController.fetchChatRooms {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func createChat(_ sender: Any) {
+        chatTextField.resignFirstResponder()
+        
+        guard let chatTitle = chatTextField.text else { return }
+        
+        chatTextField.text = ""
+        
+        chatRoomsController.createChatRoom(with: chatTitle) {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+
+        return chatRoomsController.chatRooms.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChatRoomCell", for: indexPath)
 
-        // Configure the cell...
+        cell.textLabel?.text = chatRoomsController.chatRooms[indexPath.row].title
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+    
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "ShowMessageSegue" {
+            guard let indexPath = tableView.indexPathForSelectedRow,
+                let destinationVC = segue.destination as?
+                MessageViewController else { return }
+            
+            destinationVC.chatRoomController = chatRoomsController
+            destinationVC.chatRoom = chatRoomsController.chatRooms[indexPath.row]
+        }
     }
-    */
-
 }
