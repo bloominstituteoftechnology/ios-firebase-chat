7e3555a18e9ee38a67e519f8f5e94642e378c3ad
Wilma <kima_92@fastmail.com>
2019-11-13 09:47:57 -0500

    Reconfigured ChatsTableVC and renamed MessagesDetailVC.

--- Firebase Chat/View Controllers/ChatsTableViewController.swift
+++ Firebase Chat/View Controllers/ChatsTableViewController.swift
@@ -7,84 +7,107 @@
 //
 
 import UIKit
+import MessageKit
 
 class ChatsTableViewController: UITableViewController {
-
+    
+    // MARK: - Properties
+    
+    var chatController = ChatController()
+    
+    @IBOutlet weak var threadTitleTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        if let currentUserDictionary = UserDefaults.standard.value(forKey: "currentUser") as? [String : String ] {
+            let currentUser = Sender(dictionary: currentUserDictionary)
+            chatController.currentUser = currentUser
+        } else {
+            let alert = UIAlertController(title: "Set user Name", message: nil, preferredStyle: .alert)
+            
+            var userNameTextField: UITextField!
+            
+            alert.addTextField { (textField) in
+                textField.placeholder = "Username"
+                userNameTextField = textField
+                
+                
+            }
+            
+            let submitAction = UIAlertAction(title: "Submit", style: .default) {
+                (_) in
+                
+                let displayName = userNameTextField.text ?? "Unknown user"
+                let id = UUID().uuidString
+                
+                let sender = Sender(senderId: id, displayName: displayName)
+                
+                
+                
+                UserDefaults.standard.set(sender.dictionaryRepresentation, forKey: "currentUser")
+                
+                self.chatController.currentUser = sender
+            }
+            
+            alert.addAction(submitAction)
+            present(alert, animated: true, completion: nil)
+        }
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        chatController.fetchChatRooms {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
     }
-
+    
+    // MARK: - Actions
+    
+    @IBAction func createThread(_ sender: Any) {
+        threadTitleTextField.resignFirstResponder()
+        
+        guard let threadTitle = threadTitleTextField.text else { return }
+        
+        threadTitleTextField.text = ""
+        
+        chatController.createChatRoom(with: threadTitle) {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    // MARK: - UITableViewDataSource
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return chatController.chatRooms.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MessageThreadCell", for: indexPath)
+        
+        cell.textLabel?.text = chatController.chatRooms[indexPath.row].title
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+    
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "ShowMessagesSegue" {
+            guard let indexPath = tableView.indexPathForSelectedRow,
+                let destinationVC = segue.destination as? MessagesDetailViewController else { return }
+            
+            destinationVC.chatController = chatController
+            destinationVC.chatRoom = chatController.chatRooms[indexPath.row]
+        }
     }
-    */
-
 }
+
+
+
