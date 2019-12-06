a19285bd6f2fcc829e3264449a4ad3113381280b
Robert Keffury <bobby.keffury@me.com>
2019-12-06 09:34:18 -0800

    Created New Project

---
+++ FirebaseChat/FirebaseChat/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FirebaseChat
+//
+//  Created by Bobby Keffury on 12/6/19.
+//  Copyright © 2019 Bobby Keffury. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FirebaseChat/FirebaseChat/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FirebaseChat
+//
+//  Created by Bobby Keffury on 12/6/19.
+//  Copyright © 2019 Bobby Keffury. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
