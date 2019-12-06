3abe389db09ee8430053c5735ba0cc906afcbb4c
Robert Keffury <bobby.keffury@me.com>
2019-12-06 09:42:12 -0800

    Created new project

--- FirebaseChat/FirebaseChat/ViewController.swift
+++ FirebaseChat/FirebaseChat/ViewController.swift
@@ -6,20 +6,13 @@
 //  Copyright © 2019 Bobby Keffury. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
-class ViewController: NSViewController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
     }
 
 
