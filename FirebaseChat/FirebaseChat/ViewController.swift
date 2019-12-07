c342c5b21e995b2275e373927f4a2bf65aea0699
John Kouris <jdkouris@gmail.com>
2019-12-07 08:23:25 -0800

    Added base app

---
+++ FirebaseChat/FirebaseChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseChat
+//
+//  Created by John Kouris on 12/7/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
