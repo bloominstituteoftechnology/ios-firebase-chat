5ce354883cc44740030e998e9354e909d6f01018
John Kouris <jdkouris@gmail.com>
2019-12-07 08:46:17 -0800

    Setup the basic UI with storyboard

---
+++ FirebaseChat/FirebaseChat/View Controllers/MessageViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MessageViewController.swift
+//  FirebaseChat
+//
+//  Created by John Kouris on 12/7/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import UIKit
+import MessageKit
+
+class MessageViewController: MessagesViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
