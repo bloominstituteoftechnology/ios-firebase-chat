f94d791e793fc45eed361c58eab4f74518b28f5b
Jon Bash <jonbash@protonmail.com>
2019-12-17 15:54:20 -0800

    clean up messagesVC

--- FirebaseChat/FirebaseChat/View Controllers/ChatroomViewController.swift
+++ FirebaseChat/FirebaseChat/View Controllers/ChatroomViewController.swift
@@ -11,10 +11,32 @@ import MessageKit
 import InputBarAccessoryView
 
 class ChatroomViewController: MessagesViewController {
-    var chatController: ChatController!
-    var chatroom: ChatRoom?
+    // MARK: - Properties
     
-    private func sendNewMessage(with text: String) {
+    var chatController: ChatController!
+    var chatroom: ChatRoom? {
+        didSet {
+            DispatchQueue.main.async { self.title = self.chatroom?.name }
+        }
+    }
+    
+    // MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        messageInputBar.delegate = self
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
+    }
+    
+    // MARK: - Helper Methods
+    
+    private func sendNewMessage(
+        with text: String,
+        didCompleteWithErrorMessage: @escaping (String?) -> Void)
+    {
         guard
             let sender = currentSender() as? Sender,
             !text.isEmpty
@@ -27,29 +49,23 @@ class ChatroomViewController: MessagesViewController {
             in: chatroom! // nil just checked
         ) { error in
             if let error = error {
-                NSLog("Error creating message: \(error)")
-                return
-            }
-            
-            DispatchQueue.main.async {
-                self.messagesCollectionView.reloadData()
-            }
+                didCompleteWithErrorMessage("Error creating message: \(error)")
+            } else { didCompleteWithErrorMessage(nil) }
         }
     }
     
-    private func createNewChatRoom(named text: String) {
-        guard
-            let sender = chatController.currentUser,
-            !text.isEmpty
-            else { return }
+    private func createNewChatRoom(
+        named text: String,
+        didCompleteWithErrorMessage: @escaping (String?) -> Void)
+    {
+        guard !text.isEmpty else { return }
         
-        chatController?.create(ChatRoom(
-            name: text,
-            messages: [Message(sender: sender, text: text)])
-        ) { error in
+        chatroom = ChatRoom(name: text)
+        chatController?.create(ChatRoom(name: text)) { error in
             if let error = error {
-                NSLog("Error creating new chatroom: \(error)")
-            }
+                self.chatroom = nil
+                didCompleteWithErrorMessage("Error creating new chatroom: \(error)")
+            } else { didCompleteWithErrorMessage(nil) }
         }
     }
 }
@@ -62,7 +78,10 @@ extension ChatroomViewController: MessagesDataSource {
             Sender(id: UUID().uuidString, displayName: "Unknown User")
     }
     
-    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+    func messageForItem(
+        at indexPath: IndexPath,
+        in messagesCollectionView: MessagesCollectionView
+    ) -> MessageType {
         guard let room = chatroom else {
             fatalError("No chatroom found for ChatroomViewController")
         }
@@ -73,7 +92,10 @@ extension ChatroomViewController: MessagesDataSource {
         return 1
     }
     
-    func numberOfItems(inSection section: Int, in messagesCollectionView: MessagesCollectionView) -> Int {
+    func numberOfItems(
+        inSection section: Int,
+        in messagesCollectionView: MessagesCollectionView
+    ) -> Int {
         return chatroom?.messages.count ?? 0
     }
 }
@@ -81,11 +103,19 @@ extension ChatroomViewController: MessagesDataSource {
 // MARK: - MessagesLayoutDelegate
 
 extension ChatroomViewController: MessagesLayoutDelegate {
-    func messageTopLabelHeight(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
+    func messageTopLabelHeight(
+        for message: MessageType,
+        at indexPath: IndexPath,
+        in messagesCollectionView: MessagesCollectionView
+    ) -> CGFloat {
         return 16
     }
     
-    func messageBottomLabelHeight(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
+    func messageBottomLabelHeight(
+        for message: MessageType,
+        at indexPath: IndexPath,
+        in messagesCollectionView: MessagesCollectionView
+    ) -> CGFloat {
         return 16
     }
 }
@@ -93,28 +123,64 @@ extension ChatroomViewController: MessagesLayoutDelegate {
 // MARK: - MessagesDisplayDelegate
 
 extension ChatroomViewController: MessagesDisplayDelegate {
-    func inputBar(_ inputBar: InputBarAccessoryView, didPressSendButtonWith text: String) {
+    func inputBar(
+        _ inputBar: InputBarAccessoryView,
+        didPressSendButtonWith text: String)
+    {
+        // handler for text depending on conditions
+        func creationDidComplete(withErrorMessage errorMessage: String?) {
+            if let errorMessage = errorMessage {
+                NSLog(errorMessage)
+                DispatchQueue.main.async {
+                    inputBar.inputTextView.text = text
+                }
+                return
+            }
+            DispatchQueue.main.async {
+                self.messagesCollectionView.reloadData()
+            }
+        }
+        
         if chatroom == nil {
-            createNewChatRoom(named: text)
+            createNewChatRoom(
+                named: text,
+                didCompleteWithErrorMessage: creationDidComplete(withErrorMessage:))
         } else {
-            sendNewMessage(with: text)
+            sendNewMessage(
+                with: text,
+                didCompleteWithErrorMessage: creationDidComplete(withErrorMessage:))
         }
         
         inputBar.inputTextView.text = ""
     }
     
-    func textColor(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> UIColor {
+    func textColor(
+        for message: MessageType,
+        at indexPath: IndexPath,
+        in messagesCollectionView: MessagesCollectionView
+    ) -> UIColor {
         return isFromCurrentSender(message: message) ? .white : .black
     }
     
-    func backgroundColor(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> UIColor {
+    func backgroundColor(
+        for message: MessageType,
+        at indexPath: IndexPath,
+        in messagesCollectionView: MessagesCollectionView
+    ) -> UIColor {
         return isFromCurrentSender(message: message) ? .blue : .green
     }
     
-    func configureAvatarView(_ avatarView: AvatarView, for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) {
+    func configureAvatarView(
+        _ avatarView: AvatarView,
+        for message: MessageType,
+        at indexPath: IndexPath,
+        in messagesCollectionView: MessagesCollectionView)
+    {
         let avatar = Avatar(image: nil, initials: message.sender.displayName)
         avatarView.set(avatar: avatar)
     }
 }
+
+// MARK: - MessageInputBarDelegate
 
 extension ChatroomViewController: MessageInputBarDelegate {}
