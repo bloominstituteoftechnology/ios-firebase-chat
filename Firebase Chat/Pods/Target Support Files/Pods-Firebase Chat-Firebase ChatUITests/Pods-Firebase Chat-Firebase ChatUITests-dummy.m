1ac0438cc65f117d465ea3f7e3299cb77cfc8e95
Dennis Rudolph <Dennis@Denniss-MacBook-Pro.local>
2019-12-17 17:10:07 -0500

    Parts 1-3

---
+++ Firebase Chat/Pods/Target Support Files/Pods-Firebase Chat-Firebase ChatUITests/Pods-Firebase Chat-Firebase ChatUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Firebase_Chat_Firebase_ChatUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Firebase_Chat_Firebase_ChatUITests
+@end
