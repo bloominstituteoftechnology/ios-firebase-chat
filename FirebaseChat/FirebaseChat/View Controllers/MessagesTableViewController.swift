a289d0468ce149281b93eb184d916242a9b64a27
Jon Bash <jonbash@protonmail.com>
2019-12-17 15:54:05 -0800

    set up tableVC for login

--- FirebaseChat/FirebaseChat/View Controllers/MessagesTableViewController.swift
+++ FirebaseChat/FirebaseChat/View Controllers/MessagesTableViewController.swift
@@ -7,10 +7,22 @@
 //
 
 import UIKit
+import MessageKit
 
 class MessagesTableViewController: UITableViewController {
-
+    // MARK: - Properties
+    
     var chatController = ChatController()
+    
+    // MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        chatController.attemptToLogIn { didSucceed in
+            if !didSucceed {
+                DispatchQueue.main.async { self.showNewUserAlert() }
+            }
+        }
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -24,7 +36,7 @@ class MessagesTableViewController: UITableViewController {
             }
         }
     }
-
+    
     // MARK: - Table view data source
 
     override func tableView(
@@ -58,5 +70,41 @@ class MessagesTableViewController: UITableViewController {
             roomVC.chatController = chatController
             roomVC.chatroom = chatController.chatrooms[index]
         }
+        if segue.identifier == "NewChatRoomSegue" {
+            guard
+                let roomVC = segue.destination as? ChatroomViewController
+                else { return }
+            roomVC.chatController = chatController
+        }
+    }
+    
+    // MARK: - Helper Methods
+
+    private func showNewUserAlert() {
+        let alert = UIAlertController(
+            title: "Set a username",
+            message: nil,
+            preferredStyle: .alert)
+        
+        var usernameTextField: UITextField!
+        
+        alert.addTextField { (textField) in
+            textField.placeholder = "Username:"
+            usernameTextField = textField
+        }
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { _ in
+            let displayName = usernameTextField.text ?? "No name"
+            let id = UUID().uuidString
+            
+            let sender = Sender(senderId: id, displayName: displayName)
+            
+            UserDefaults.standard.set(sender.dictionaryRepresentation, forKey: "currentUser")
+            
+            self.chatController.login(with: sender)
+        }
+
+        alert.addAction(submitAction)
+        present(alert, animated: true, completion: nil)
     }
 }
