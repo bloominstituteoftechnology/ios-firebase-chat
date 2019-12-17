f59c8d3a5dcaf697a7f832e39bf014826a999a44
Jon Bash <jonbash@protonmail.com>
2019-12-17 11:52:01 -0800

    init project, gitignore

---
+++ FirebaseChat/FirebaseChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseChat
+//
+//  Created by Jon Bash on 2019-12-17.
+//  Copyright © 2019 Jon Bash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
