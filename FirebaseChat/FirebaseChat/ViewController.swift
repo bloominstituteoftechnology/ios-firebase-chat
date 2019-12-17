de037a022cfc36e3ee68d92e54971a59f1f232df
Nar Kumar <njn.kumar@gmail.com>
2019-12-17 15:13:43 -0800

    CocoaPods, Firebase, MessageKit All Added

---
+++ FirebaseChat/FirebaseChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseChat
+//
+//  Created by Niranjan Kumar on 12/17/19.
+//  Copyright © 2019 Nar Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
