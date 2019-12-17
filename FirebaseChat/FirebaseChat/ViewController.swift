806a555559de407c08e35517f5cd0460e608dbd8
Skyler Suzuki <skylersuzuk1@gmail.com>
2019-12-17 13:09:38 -0800

    Created Firebase project. Added firebase and messagekit frameworks to project.

---
+++ FirebaseChat/FirebaseChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseChat
+//
+//  Created by Lambda_School_Loaner_204 on 12/17/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
