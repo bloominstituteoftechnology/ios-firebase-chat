566e0e850056e49feab146cb53a2bab51ee9aef2
Nar Kumar <njn.kumar@gmail.com>
2019-12-17 17:01:20 -0800

    Models WIP

--- FirebaseChat/FirebaseChat/AppDelegate.swift
+++ FirebaseChat/FirebaseChat/AppDelegate.swift
@@ -12,9 +12,7 @@ import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
