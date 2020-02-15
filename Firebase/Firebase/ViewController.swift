6b29c239ab80a7db8ea633fcaae4db94d9d342ec
Alex Thompson <guitarman226@gmail.com>
2020-02-15 15:49:52 -0600

    Making an awesome commit

---
+++ Firebase/Firebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Firebase
+//
+//  Created by Lambda_School_Loaner_213 on 2/15/20.
+//  Copyright © 2020 Lambda_School_Loaner_213. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
