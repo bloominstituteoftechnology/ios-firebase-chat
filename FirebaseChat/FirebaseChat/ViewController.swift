f2923ee8e2b4d16c74b2002bfb412c965c599cb2
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-02-25 14:06:39 -0800

    created project

---
+++ FirebaseChat/FirebaseChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseChat
+//
+//  Created by Jorge Alvarez on 2/25/20.
+//  Copyright © 2020 Jorge Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hi")
+    }
+
+
+}
+
