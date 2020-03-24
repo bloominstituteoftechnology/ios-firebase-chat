6a9f945cb7d4475e1e0219f23d00d4711a5c90ad
Nick <ptnguyen1901@gmail.com>
2020-03-24 15:01:54 -0400

    Initial commit

---
+++ Chat/Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by Nick Nguyen on 3/24/20.
+//  Copyright © 2020 Nick Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
