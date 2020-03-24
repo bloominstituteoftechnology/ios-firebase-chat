b38e3706edb957aff28cba304899c02c7ef75511
Enrique Gongora <EGTech24@gmail.com>
2020-03-24 14:54:56 -0700

    ModelControllers added

---
+++ FireBaseChat/FireBaseChat/Model Controller/ChatRoomController.swift
@@ -0,0 +1,9 @@
+//
+//  ChatRoomController.swift
+//  FireBaseChat
+//
+//  Created by Enrique Gongora on 3/24/20.
+//  Copyright © 2020 Enrique Gongora. All rights reserved.
+//
+
+import Foundation
