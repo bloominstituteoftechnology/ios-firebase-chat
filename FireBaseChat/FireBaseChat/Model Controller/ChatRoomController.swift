c77dd1998a1a085d9ae6b6df745c548bb8616a86
Enrique Gongora <EGTech24@gmail.com>
2020-03-24 15:05:24 -0700

    DetailViewController Completed

--- FireBaseChat/FireBaseChat/Model Controller/ChatRoomController.swift
+++ FireBaseChat/FireBaseChat/Model Controller/ChatRoomController.swift
@@ -6,4 +6,63 @@
 //  Copyright © 2020 Enrique Gongora. All rights reserved.
 //
 
+import CodableFirebase
+import Firebase
 import Foundation
+import MessageKit
+
+class ChatRoomController {
+    
+    // --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    // MARK: - Properties
+    var rooms = [ChatRoom]()
+    let chatRoomRef = Database.database().reference().child("chatRooms")
+    
+    func fetchRooms(completion: @escaping () -> ()) {
+        chatRoomRef.observe(.value) { snapshot in
+            let decoder = FirebaseDecoder()
+            let rooms = try! decoder.decode(Rooms.self, from: snapshot.value as Any)
+            self.rooms = rooms.rooms
+            completion()
+        }
+    }
+    
+    func addRoom(with name: String, completion: @escaping () -> ()) {
+        let room = ChatRoom(id: UUID().uuidString, messages: [], name: name)
+        self.rooms.append(room)
+        updateDatabase()
+        completion()
+    }
+    
+    func deleteRoom(_ room: ChatRoom, completion: @escaping () -> ()) {
+        guard let index = rooms.firstIndex(of: room) else { return }
+        rooms.remove(at: index)
+        updateDatabase()
+        completion()
+    }
+    
+    func addMessageToRoom(_ room: ChatRoom, message: Message, completion: @escaping () -> ()) {
+        guard let index = rooms.firstIndex(of: room) else { return }
+        
+        var messageArray = [Message]()
+        
+        if let messages = self.rooms[index].messages {
+            messageArray = messages
+            messageArray.append(message)
+        } else {
+            messageArray = [message]
+        }
+        
+        rooms[index].messages = messageArray
+        
+        updateDatabase()
+        completion()
+    }
+    
+    private func updateDatabase() {
+        let rooms = Rooms(rooms: self.rooms)
+        let encoder = FirebaseEncoder()
+        let roomsData = try? encoder.encode(rooms)
+        chatRoomRef.setValue(roomsData)
+    }
+}
--- FireBaseChat/FireBaseChat/View Controllers/ChatRoomDetailViewController.swift
+++ FireBaseChat/FireBaseChat/View Controllers/ChatRoomDetailViewController.swift
@@ -11,22 +11,54 @@ import MessageKit
 import InputBarAccessoryView
 
 class ChatRoomDetailViewController: MessagesViewController {
-
+    
+    var room: ChatRoom?
+    var chatRoomController: ChatRoomController?
+    
+    var messages = [Message]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
+        messageInputBar.delegate = self
+        chatRoomController?.fetchRooms {
+            guard let room = self.room, let index = self.chatRoomController?.rooms.firstIndex(of: room) else { return }
+            self.messages = self.chatRoomController?.rooms[index].messages ?? []
+            self.messagesCollectionView.reloadData()
+        }
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension ChatRoomDetailViewController: InputBarAccessoryViewDelegate {
+    func inputBar(_ inputBar: InputBarAccessoryView, didPressSendButtonWith text: String) {
+        guard let chatRoomController = chatRoomController, let room = room else { return }
+        let message = Message(text: text, displayName: "User", messageId: UUID().uuidString, sentDate: Date())
+        self.messages.append(message)
+        chatRoomController.addMessageToRoom(room, message: message) {
+            DispatchQueue.main.async {
+                self.messagesCollectionView.reloadData()
+            }
+        }
+    }
+}
+
+extension ChatRoomDetailViewController: MessagesDataSource, MessagesLayoutDelegate, MessagesDisplayDelegate {
+    func currentSender() -> SenderType {
+        return Sender(senderId: UUID().uuidString, displayName: "User")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        let message = messages[indexPath.item]
+        return message
     }
-    */
-
+    
+    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+        return 1
+    }
+    
+    func numberOfItems(inSection section: Int, in messagesCollectionView: MessagesCollectionView) -> Int {
+        return messages.count
+    }
 }
