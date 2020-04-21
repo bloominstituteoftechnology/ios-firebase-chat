c4a628bbace7c98745b75ffc4e9d732bed2e7354
Christopher DeVito <christopher.devito@protonmail.com>
2020-04-21 15:37:49 -0400

    Initial commit. Created FirebaseChat project.

---
+++ FirebaseChat/FirebaseChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseChat
+//
+//  Created by Christopher Devito on 4/21/20.
+//  Copyright © 2020 Christopher Devito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FirebaseChat/FirebaseChatUITests/FirebaseChatUITests.swift
@@ -0,0 +1,23 @@
+//
+//  FirebaseChatUITests.swift
+//  FirebaseChatUITests
+//
+//  Created by Christopher Devito on 4/21/20.
+//  Copyright © 2020 Christopher Devito. All rights reserved.
+//
+
+import XCTest
+
+class FirebaseChatUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+
+        continueAfterFailure = false
+
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
