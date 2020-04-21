5f110977a2efc0685cc0c4c8dd45bb6e1ed14399
Wyatt Harrell <wyattaharrell@gmail.com>
2020-04-21 16:23:05 -0400

    View controllers created

---
+++ Firebase Chat/Firebase Chat/ChatViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ChatViewController.swift
+//  Firebase Chat
+//
+//  Created by Wyatt Harrell on 4/21/20.
+//  Copyright © 2020 Wyatt Harrell. All rights reserved.
+//
+
+import UIKit
+import MessageKit
+import InputBarAccessoryView
+
+class ChatViewController: MessagesViewController, InputBarAccessoryViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
