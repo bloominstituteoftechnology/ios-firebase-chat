fc449734f575a9e10f010ccb02c7dd85268df5d2
Tobi Tselios <tobi.tselios@gmail.com>
2020-04-21 23:08:52 +0100

    Installed Firebase CocoaPod

---
+++ Firebase Chat/Firebase Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Firebase Chat
+//
+//  Created by Tobi Kuyoro on 21/04/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
