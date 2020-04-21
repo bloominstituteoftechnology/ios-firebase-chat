b6249db11e2cbc6897f7553847ed42ed14b4374c
Lydia Zhang <art4everyan@gmail.com>
2020-04-21 19:13:45 -0400

    Has error no Messa 'no messagekit module

---
+++ Firebase_Chat/Firebase_Chat/ChatRoomController.swift
@@ -0,0 +1,18 @@
+//
+//  ChatRoomController.swift
+//  Firebase_Chat
+//
+//  Created by Lydia Zhang on 4/21/20.
+//  Copyright © 2020 Lydia Zhang. All rights reserved.
+//
+
+import Foundation
+import MessageKit
+import Firebase
+
+class ChatRoomController {
+    
+    func fetchChatRooms(completion: @escaping () -> ()) {
+
+    }
+}
