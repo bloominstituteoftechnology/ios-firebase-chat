fa8400024512a8df36c870729a8e9ef91fb80973
Shawn Gee <shawngee@gmail.com>
2020-04-22 00:33:18 -0400

    Prepare for segue to message vc

--- FirebaseChat/FirebaseChat/ViewControllers/ChatRoomTableViewController.swift
+++ FirebaseChat/FirebaseChat/ViewControllers/ChatRoomTableViewController.swift
@@ -20,10 +20,10 @@ class ChatRoomTableViewController: UITableViewController {
     }
     
     @IBAction func addButtonTapped(_ sender: Any) {
-        chatRoomController.createChatRoom(name: "A Testing")
+        chatRoomController.createChatRoom(name: "B Testing")
     }
 
-    // MARK: - Table view data source
+    // MARK: - Table View Data Source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         chatRoomController.chatRooms.count
@@ -37,51 +37,15 @@ class ChatRoomTableViewController: UITableViewController {
 
         return cell
     }
-    
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "ShowMessagesSegue",
+            let messageVC = segue.destination as? MessageViewController,
+            let indexPath = tableView.indexPathForSelectedRow {
+            messageVC.chatRoom = chatRoomController.chatRooms[indexPath.row]
+        }
     }
-    */
 
 }
