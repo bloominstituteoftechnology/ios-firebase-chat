3ead85ec01f31725cb02862cb56436f48435bc20
David Wright <davidtwright89@gmail.com>
2020-04-24 01:17:48 -0400

    Added the Firebase SDK

--- Firebase Chat/Firebase Chat/AppDelegate.swift
+++ Firebase Chat/Firebase Chat/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // Use Firebase library to configure APIs
+        FirebaseApp.configure()
+        
         return true
     }
 
