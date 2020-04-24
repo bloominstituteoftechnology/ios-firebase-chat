7f25188abd4df7ee2199fa5e28ee8a4bab080b64
David Wright <davidtwright89@gmail.com>
2020-04-24 00:36:27 -0400

    Created a new Xcode project named: "Firebase Chat"

---
+++ Firebase Chat/Firebase Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Firebase Chat
+//
+//  Created by David Wright on 4/24/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
