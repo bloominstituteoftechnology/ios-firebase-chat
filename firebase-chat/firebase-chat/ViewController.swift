1c0b60a547eed2c2ff270dbbd0c61e8282d58bcb
Aaron Haughton <dreamcast2000@outlook.com>
2020-04-25 12:31:24 -0400

    Initial Commit; Chat app created.

---
+++ firebase-chat/firebase-chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firebase-chat
+//
+//  Created by Joe on 4/25/20.
+//  Copyright © 2020 AlphaGradeINC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
