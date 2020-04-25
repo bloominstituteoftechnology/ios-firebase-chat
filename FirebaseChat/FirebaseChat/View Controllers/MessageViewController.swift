93ef062ddf4fb3026ad194c411d656631f70b831
Jessie <jessie.griffin@icloud.com>
2020-04-24 23:44:12 -0400

    Adds storyboard features

---
+++ FirebaseChat/FirebaseChat/View Controllers/MessageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MessageViewController.swift
+//  FirebaseChat
+//
+//  Created by Jessie Ann Griffin on 4/24/20.
+//  Copyright © 2020 Jessie Griffin. All rights reserved.
+//
+
+import UIKit
+
+class MessageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
