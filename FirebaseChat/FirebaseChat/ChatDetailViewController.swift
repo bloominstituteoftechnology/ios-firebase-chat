322788111c6f7640c8b07f86cc49600f66ec042e
Sal <salmanamer@gmail.com>
2020-04-24 20:33:50 -0700

    setup project, setup firebase, setup message kit, setup tableviewcontroller and detailviewcontroller

---
+++ FirebaseChat/FirebaseChat/ChatDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChatDetailViewController.swift
+//  FirebaseChat
+//
+//  Created by Sal B Amer on 4/24/20.
+//  Copyright © 2020 Sal B Amer. All rights reserved.
+//
+
+import UIKit
+
+class ChatDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
