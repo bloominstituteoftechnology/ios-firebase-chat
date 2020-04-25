5ba59e217dcdd991c22fafc427f88261f6ea7d88
Ezra Black <dev.ezrablack@gmail.com>
2020-04-25 01:15:25 -0500

    Base Project.

---
+++ FireChat/FireChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FireChat
+//
+//  Created by Ezra Black on 4/25/20.
+//  Copyright © 2020 Casanova Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FireChat/Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FireChat' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FireChat
+  pod 'Firebase/Analytics'
+end
