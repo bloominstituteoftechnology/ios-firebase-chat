7c9defcb251f6c6ea1600474fa051dfa2b674de1
Aaron Haughton <dreamcast2000@outlook.com>
2020-04-25 14:54:24 -0400

    End of Day 5 Commit

--- firebase-chat/firebase-chat/Models/Message.swift
+++ firebase-chat/firebase-chat/Models/Message.swift
@@ -7,10 +7,32 @@
 //
 
 import Foundation
+import MessageKit
 
 class Message {
+    public enum MessageKind {
+        case text(String) // TextMessageCell
+        case attributedText(NSAttributedString) // TextMessageCell
+        case photo(MediaItem) // MediaMessageCell
+        case video(MediaItem) // MediaMessageCell
+        case location(LocationItem) // LocationMessageCell
+        case emoji(String) // TextMessageCell
+        case audio(AudioItem) // AudioMessageCell
+        case contact(ContactItem) // ContactMessageCell
+
+        case custom(Any?)
+    }
     
-    
-    
-    
+}
+
+
+public protocol MessageType {
+
+    var sender: Sender { get }
+
+    var messageId: String { get }
+
+    var sentDate: Date { get }
+
+    var kind: MessageKind { get }
 }
