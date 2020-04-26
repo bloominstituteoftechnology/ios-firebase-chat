f327d370d7595a27871cd7805f1c4874f07140f8
David Wright <davidtwright89@gmail.com>
2020-04-26 03:21:30 -0400

    Implemented create and fetch methods for chatRooms and messages

--- Firebase Chat/Firebase Chat/Model Controller/ChatRoomController.swift
+++ Firebase Chat/Firebase Chat/Model Controller/ChatRoomController.swift
@@ -15,26 +15,44 @@ class ChatRoomController {
     // MARK: Properties
 
     var chatRooms: [ChatRoom] = []
-    var currentUser: Sender?
+    var currentUser: Sender? = Sender(senderId: UUID().uuidString, displayName: "Default User")
     
     var ref: DatabaseReference!
     
     // MARK: Methods
 
     func fetchChatRooms(completion: @escaping () -> Void) {
-        // TODO: Implement fetchChatRooms()
+        // TODO: Test fetchChatRooms()
+        ref.child("chatRooms").observe(DataEventType.value, with: { snapshot in
+            let chatDict = snapshot.value as? [String : ChatRoom] ?? [:]
+            self.chatRooms = Array(chatDict.values)
+            completion()
+        })
     }
     
     func fetchMessages(in chatRoom: ChatRoom, completion: @escaping () -> Void) {
-        // TODO: Implement fetchMessages()
+        // TODO: Test fetchMessages()
+        ref.child("messages").child(chatRoom.id).child("messages").observe(DataEventType.value, with: { snapshot in
+            let messagesDict = snapshot.value as? [String : Message] ?? [:]
+            chatRoom.messages = Array(messagesDict.values)
+            completion()
+        })
     }
     
     func createChatRoom(titled title: String, completion: @escaping () -> Void) {
-        // TODO: Implement createChatRoom()
+        // TODO: Test createChatRoom()
+        let chatRoom = ChatRoom(title: title)
+        chatRooms.append(chatRoom)
+        self.ref.child("chatRooms").setValue([chatRoom.id: chatRoom])
+        completion()
     }
     
     func createMessage(in chatRoom: ChatRoom, withText text: String, from sender: Sender, completion: @escaping () -> Void) {
-        // TODO: Implement createMessage()
+        // TODO: Test createMessage()
+        let message = Message(text: text, sender: sender)
+        chatRoom.messages.append(message)
+        self.ref.child("messages").child(chatRoom.id).child("messages").setValue([message.messageID: message])
+        completion()
     }
     
     // MARK: - Initializers
--- Firebase Chat/Firebase Chat/Models/ChatRoom.swift
+++ Firebase Chat/Firebase Chat/Models/ChatRoom.swift
@@ -9,7 +9,7 @@
 import Foundation
 //import MessageKit
 
-struct ChatRoom: Codable {
+class ChatRoom: Codable {
     
     let title: String
     var messages: [Message]
