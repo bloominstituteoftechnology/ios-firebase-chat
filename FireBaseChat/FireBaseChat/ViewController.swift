05d1e1ef071b5fd07c46dde584e1228a7f174b59
Denis Cedeno <denis.cedeno@gmail.com>
2020-04-26 20:23:03 -0400

    added google service plist

---
+++ FireBaseChat/FireBaseChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FireBaseChat
+//
+//  Created by denis cedeno on 4/26/20.
+//  Copyright © 2020 DenCedeno Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
