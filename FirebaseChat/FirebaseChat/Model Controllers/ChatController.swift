77437ee3c55abfc8d343735489376cb5b9ff095d
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-04-26 22:04:10 -0400

    Added CodableFirebase pod

---
+++ FirebaseChat/FirebaseChat/Model Controllers/ChatController.swift
@@ -0,0 +1,19 @@
+//
+//  ChatController.swift
+//  FirebaseChat
+//
+//  Created by Joshua Rutkowski on 4/26/20.
+//  Copyright © 2020 Josh Rutkowski. All rights reserved.
+//
+
+
+import CodableFirebase
+import Foundation
+import Firebase
+import MessageKit
+
+class ChatController {
+    var rooms = [ChatRoom]()
+    
+    
+}
