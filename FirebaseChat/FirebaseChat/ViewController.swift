8b4e8a8035b79a6f3e1ce5ba86c4a251d83625fd
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-04-26 21:11:29 -0400

    Initial commit

---
+++ FirebaseChat/FirebaseChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseChat
+//
+//  Created by Joshua Rutkowski on 4/26/20.
+//  Copyright © 2020 Josh Rutkowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
