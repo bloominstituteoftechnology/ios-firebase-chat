a9669583b0b00431c92707ff7d8a1e62a0297cec
Shawn James <shawn.james@me.com>
2020-05-19 17:47:48 -0400

    Add skeleton prepare for seg, and tableview data source

--- FirebaseChat/FirebaseChat/ViewControllers/ConversationsViewController.swift
+++ FirebaseChat/FirebaseChat/ViewControllers/ConversationsViewController.swift
@@ -12,19 +12,24 @@ class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+// MARK: - Navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+//         Get the new view controller using segue.destination.
+//         Pass the selected object to the new view controller.
     }
-    */
 
+}
+
+extension ConversationsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 }
