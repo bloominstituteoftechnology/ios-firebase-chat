fe0aaed7cae4eadee1e9277d5201e0f3b7a360ae
Shawn James <shawn.james@me.com>
2020-05-19 15:59:52 -0400

    Add Firebase framework to the project

--- FirebaseChat/FirebaseChat/AppDelegate.swift
+++ FirebaseChat/FirebaseChat/AppDelegate.swift
@@ -8,14 +8,14 @@
 
 import UIKit
 import CoreData
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
