8014a0851e0933f11275710d79829eeb9df71475
Shawn James <shawn.james@me.com>
2020-05-19 17:56:42 -0400

    Add skeleton methods for the model controller

--- FirebaseChat/FirebaseChat/Models/ModelController.swift
+++ FirebaseChat/FirebaseChat/Models/ModelController.swift
@@ -7,9 +7,29 @@
 //
 
 import Foundation
+import FirebaseDatabase
 
 class ModelController {
     
+    // MARK: - Methods
+//    Create a chat room in Firebase
+    func addNewConversation() {
+        
+    }
     
+//    Fetch chat rooms from Firebase
+    func fetchConversationsFromFirebase() {
+        
+    }
+    
+//    Create a message in a chat room in Firebase
+    func addNewMessage() {
+        
+    }
+    
+//    Fetch messages in a chat room from Firebase
+    func fetchMessagesFromFirebase() {
+        
+    }
     
 }
