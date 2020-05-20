18c014ee54b0f8ec150329f39e379b9cb40f32e9
Chris Dobek <dobek.christopher@gmail.com>
2020-05-20 16:41:58 -0400

    Storyboard updated and files created

---
+++ Chat/Chat/View Controllers/MessageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MessageViewController.swift
+//  Chat
+//
+//  Created by Chris Dobek on 5/20/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+import UIKit
+
+class MessageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
