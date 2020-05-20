61b74a3c9f7b906d2671e559ce651c41af7b4c9f
Chris Dobek <dobek.christopher@gmail.com>
2020-05-20 15:13:22 -0400

    Project Created

---
+++ Chat/Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by Chris Dobek on 5/20/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
