844b34c2220db05c2e8018db1704ebc53a68a84c
Shawn James <shawn.james@me.com>
2020-05-19 21:43:17 -0400

    Moved everything into the viewController to simplify the project. Adding fetching functionality

--- FirebaseChat/FirebaseChat/ViewControllers/ConversationsViewController.swift
+++ FirebaseChat/FirebaseChat/ViewControllers/ConversationsViewController.swift
@@ -7,36 +7,60 @@
 //
 
 import UIKit
+import Firebase
 
 class ConversationsViewController: UIViewController {
-
+    
     // MARK: - Outlets & Properties
     @IBOutlet weak var newConversationTitleTextField: UITextField!
-    let modelController = ModelController()
+    @IBOutlet weak var tableView: UITableView!
+    let databaseReference = Database.database().reference()
+    var conversations = [String]()
+    
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        fetchNewConversations()
     }
     
     // MARK: - Actions & Methods
     @IBAction func addNewConversationButtonPressed(_ sender: UIButton) {
+        newConversationTitleTextField.resignFirstResponder()
         guard let name = newConversationTitleTextField.text, !name.isEmpty else { return }
-        modelController.addNewConversation(named: name)
+        databaseReference.child("Conversations").childByAutoId().setValue((["name" : name]))
+        newConversationTitleTextField.text = ""
+    }
+    
+    func fetchNewConversations() {
+        databaseReference.child("Conversations").observe(.childAdded) { (dataSnapshot) in
+            guard let name = dataSnapshot.childSnapshot(forPath: "name").value as? String else { return }
+            self.conversations.append(name)
+            self.tableView.reloadData()
+        }
     }
     
     // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//         Get the new view controller using segue.destination.
-//         Pass the selected object to the new view controller.
+        guard segue.identifier == "ShowMessages", let indexPath = tableView.indexPathForSelectedRow else { return }
+        let messageViewController = segue.destination as! MessageViewController
+        messageViewController.conversation = conversations[indexPath.row]
     }
-
+    
 }
 
 // MARK: - TableView data source
 extension ConversationsViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { 0 }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        conversations.count
+    }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell { return UITableViewCell() }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ConversationCell", for: indexPath)
+        cell.textLabel?.text = conversations[indexPath.row]
+        cell.detailTextLabel?.text = ""
+        return cell
+    }
     
 }
