a4b51d6a5fb011e3d974c36c7ac3c34fa5394fcf
Chris Dobek <dobek.christopher@gmail.com>
2020-05-20 16:44:02 -0400

    Added MessageThreadController file

---
+++ Chat/Chat/Model Controller/MessageThreadController.swift
@@ -0,0 +1,13 @@
+//
+//  MessageThreadController.swift
+//  Chat
+//
+//  Created by Chris Dobek on 5/20/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+import Foundation
+
+class MessageThreadController {
+    
+}
