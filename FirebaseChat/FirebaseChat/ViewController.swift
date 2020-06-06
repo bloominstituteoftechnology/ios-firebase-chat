95dc762e896fcc789be777ce819f2faa1bc5f6dc
Claudia Maciel <clc_80@yahoo.com>
2020-06-05 20:33:51 -0700

    trying to install firebase

---
+++ FirebaseChat/FirebaseChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseChat
+//
+//  Created by Claudia Contreras on 6/5/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
