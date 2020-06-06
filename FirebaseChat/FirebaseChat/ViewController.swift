6e21d799e9e06d77eeb33651fde5543d8de0fd07
Chad Parker <chad@chadparker.com>
2020-06-05 20:20:43 -0700

    Add Xcode project

---
+++ FirebaseChat/FirebaseChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseChat
+//
+//  Created by Chad Parker on 6/5/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
