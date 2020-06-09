c9cc0ae3476e6371695dfd4c05fbca1524a10a6b
Chad Parker <chad@chadparker.com>
2020-06-09 15:52:47 -0700

    Create Message and ChatRoom model objects

---
+++ FirebaseChat/FirebaseChat/Model/ChatRoom.swift
@@ -0,0 +1,15 @@
+//
+//  ChatRoom.swift
+//  FirebaseChat
+//
+//  Created by Chad Parker on 2020-06-09.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+import Foundation
+
+struct ChatRoom {
+   
+   let name: String
+   let messages: [Message]
+}
---
+++ FirebaseChat/FirebaseChat/Model/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  FirebaseChat
+//
+//  Created by Chad Parker on 2020-06-09.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+   
+   let text: String
+}
