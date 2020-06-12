93ec8cfa61634b738d5cb8f4e80428940354892a
Chad Parker <chad@chadparker.com>
2020-06-12 07:59:54 -0700

    Create MessageVC

---
+++ FirebaseChat/FirebaseChat/View Controllers/MessageVC.swift
@@ -0,0 +1,18 @@
+//
+//  MessageVC.swift
+//  FirebaseChat
+//
+//  Created by Chad Parker on 2020-06-09.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+import UIKit
+
+class MessageVC: UIViewController {
+   
+   var chatRoom: ChatRoom!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
