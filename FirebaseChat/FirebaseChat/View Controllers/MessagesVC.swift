f9f2ea83b34b191fb5829fd23c16f12b962a47e1
Chad Parker <chad@chadparker.com>
2020-06-15 07:31:32 -0700

    Implement MessagesVC, messages displaying, can input messages

--- FirebaseChat/FirebaseChat/View Controllers/MessagesVC.swift
+++ FirebaseChat/FirebaseChat/View Controllers/MessagesVC.swift
@@ -7,22 +7,30 @@
 //
 
 import UIKit
+import MessageKit
+import InputBarAccessoryView
 
-class MessagesVC: UIViewController {
+class MessagesVC: MessagesViewController {
    
    var chatRoom: ChatRoom!
    var modelController: ModelController!
    
    private var messages: [Message] = [] {
       didSet {
-         print(messages)
+         messagesCollectionView.reloadData()
       }
    }
+   
+   private let sender = Sender(senderId: UUID().uuidString, displayName: "Chad")
    
    override func viewDidLoad() {
       super.viewDidLoad()
       
       title = chatRoom.name
+      messagesCollectionView.messagesDataSource = self
+      messagesCollectionView.messagesLayoutDelegate = self
+      messagesCollectionView.messagesDisplayDelegate = self
+      messageInputBar.delegate = self
       loadMessages()
    }
    
@@ -30,5 +38,39 @@ class MessagesVC: UIViewController {
       modelController.fetchMessagesInChatRoom(chatRoom) { messages in
          self.messages = messages
       }
+   }
+}
+
+extension MessagesVC: MessagesDataSource {
+   
+   func currentSender() -> SenderType {
+      return sender
+   }
+   
+   func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+      // MessageKit requires us to use 1 section per message
+      // https://github.com/MessageKit/MessageKit/blob/master/Documentation/QuickStart.md
+      return messages[indexPath.section]
+   }
+   
+   func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+      return messages.count
+   }
+}
+
+extension MessagesVC: MessagesLayoutDelegate {
+   
+}
+
+extension MessagesVC: MessagesDisplayDelegate {
+   
+}
+
+extension MessagesVC: InputBarAccessoryViewDelegate {
+   
+   func inputBar(_ inputBar: InputBarAccessoryView, didPressSendButtonWith text: String) {
+      modelController.createMessageInChatRoom(message: Message(text: text), chatRoom: chatRoom)
+      inputBar.inputTextView.text = nil
+      loadMessages()
    }
 }
