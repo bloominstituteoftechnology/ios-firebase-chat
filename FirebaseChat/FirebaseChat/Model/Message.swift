fc2cc50dd514a81ec9ff1b836ffa181cf5887dfc
Chad Parker <chad@chadparker.com>
2020-06-14 17:52:45 -0700

    Create Message struct with custom initializers

--- FirebaseChat/FirebaseChat/Model/Message.swift
+++ FirebaseChat/FirebaseChat/Model/Message.swift
@@ -10,5 +10,22 @@ import Foundation
 
 struct Message {
    
+   let id: String
    let text: String
+   let date: Date
+   
+   init(id: String = UUID().uuidString, text: String, date: Date = Date()) {
+      self.id = id
+      self.text = text
+      self.date = date
+   }
+   
+   init?(id: String, dict: [String: Any]) {
+      guard let text = dict["text"] as? String else { return nil }
+      
+      self.id = id
+      self.text = text
+      #warning("date needs to be parsed")
+      self.date = Date()
+   }
 }
