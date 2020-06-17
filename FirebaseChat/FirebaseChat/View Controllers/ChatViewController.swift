f114b3da3197caa7b007f5905b92c5a9daf89c79
Cody Morley <codymorleyinc@gmail.com>
2020-06-16 19:51:55 -0700

    think I finished the tableviews

---
+++ FirebaseChat/FirebaseChat/View Controllers/ChatViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChatViewController.swift
+//  FirebaseChat
+//
+//  Created by Cody Morley on 6/16/20.
+//  Copyright © 2020 Cody Morley. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FirebaseChat/FirebaseChat/Views/ChatRoomTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ChatRoomTableViewCell.swift
+//  FirebaseChat
+//
+//  Created by Cody Morley on 6/16/20.
+//  Copyright © 2020 Cody Morley. All rights reserved.
+//
+
+import UIKit
+
+class ChatRoomTableViewCell: UITableViewCell {
+    @IBOutlet weak var roomNameLabel: UILabel!
+    @IBOutlet weak var lastActiveUserLabel: UILabel!
+    @IBOutlet weak var lastActiveTimeLabel: UILabel!
+    
+}
