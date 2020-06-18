0f74d5d0c1fa0200e637497da79e8f9dc500cb1e
Harmony Radley <harmonyradley99@gmail.com>
2020-06-17 22:03:09 -0400

    Added podfile and infoplist

---
+++ FIREBASECHAT/FIREBASECHAT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FIREBASECHAT
+//
+//  Created by Harmony Radley on 6/17/20.
+//  Copyright © 2020 Harmony Radley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
