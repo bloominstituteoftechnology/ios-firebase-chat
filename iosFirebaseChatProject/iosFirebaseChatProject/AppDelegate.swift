d010314f22f945ab9df098128eaa7d27b2fc8f39
BrysonSaclausa <brys1saclaus2@gmail.com>
2020-10-03 12:48:29 -0700

    Restart project

--- iosFirebaseChatProject/iosFirebaseChatProject/AppDelegate.swift
+++ iosFirebaseChatProject/iosFirebaseChatProject/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    //helloWorld
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
