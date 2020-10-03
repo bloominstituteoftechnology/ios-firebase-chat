d61baee475c9e94ec5fadcf098b72157adc48e3a
Cora Jacobson <cora-jacobson@lambdastudents.com>
2020-10-03 09:09:59 -0700

    Part 1

---
+++ FirebaseChat/FirebaseChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseChat
+//
+//  Created by Cora Jacobson on 10/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
