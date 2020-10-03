b0d1758917228d623610a940bdbcbc09b0621cbb
Kenneth Jones <kennethjones1991@outlook.com>
2020-10-02 21:09:32 -0600

    Initial commit

---
+++ FirebaseChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseChat
+//
+//  Created by Kenneth Jones on 10/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
