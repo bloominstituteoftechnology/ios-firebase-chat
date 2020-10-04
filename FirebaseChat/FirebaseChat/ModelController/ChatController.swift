76b7ee17fe98313263a531ebe27365c6836d8fb3
Cora Jacobson <cora-jacobson@lambdastudents.com>
2020-10-04 00:08:12 -0700

    Part 4

---
+++ FirebaseChat/FirebaseChat/ModelController/ChatController.swift
@@ -0,0 +1,112 @@
+//
+//  ChatController.swift
+//  FirebaseChat
+//
+//  Created by Cora Jacobson on 10/3/20.
+//
+
+import Foundation
+import Firebase
+import FirebaseDatabase
+
+class ChatController {
+    
+    var chatRooms: [ChatRoom] = []
+    
+    let ref = Database.database().reference()
+    
+    func createChatRoom(title: String) {
+        let chatRoom = ChatRoom(title: title)
+        let chatId = chatRoom.identifier
+        ref.child(chatId).setValue(["identifier": chatId, "title": title])
+        chatRooms.append(chatRoom)
+    }
+    
+    func deleteChatRoom(chatRoom: ChatRoom) {
+        let chatId = chatRoom.identifier
+        ref.child(chatId).removeValue()
+        if let index = chatRooms.firstIndex(of: chatRoom) {
+            chatRooms.remove(at: index)
+        }
+    }
+    
+    func createMessage(chatRoom: ChatRoom, sender: Sender, text: String) {
+        let chatId = chatRoom.identifier
+        guard let index = chatRooms.firstIndex(of: chatRoom) else { return }
+        let message = Message(text: text, sender: sender)
+        let messageId = message.messageId
+        do {
+            let messageData = try JSONEncoder().encode(message)
+            let dictionary = try JSONSerialization.jsonObject(with: messageData, options: []) as? NSDictionary
+            ref.child("\(chatId)/messages/\(messageId)").setValue(dictionary)
+            chatRooms[index].messages.append(message)
+        } catch {
+            NSLog("Error encoding message: \(error)")
+        }
+    }
+    
+    func deleteMessage(chatRoom: ChatRoom, message: Message) {
+        let chatId = chatRoom.identifier
+        let messageId = message.messageId
+        ref.child(chatId).child("messages").child(messageId).removeValue()
+        if let roomIndex = chatRooms.firstIndex(of: chatRoom),
+           let index = chatRooms[roomIndex].messages.firstIndex(of: message) {
+            chatRooms[roomIndex].messages.remove(at: index)
+        }
+    }
+    
+    func fetchEverything(completion: @escaping () -> Void) {
+        fetchChatRooms { (rooms) in
+            self.chatRooms = rooms
+            for room in self.chatRooms {
+                self.fetchMessages(chatRoom: room) { (messages) in
+                    room.messages = messages
+                }
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                completion()
+            }
+        }
+    }
+    
+    func fetchChatRooms(completion: @escaping ([ChatRoom]) -> Void) {
+        var rooms: [ChatRoom] = []
+        ref.observeSingleEvent(of: .value) { (snapshot) in
+            for child in snapshot.children {
+                let snap = child as? DataSnapshot
+                let value = snap?.value as? NSDictionary
+                if let title = value?["title"] as? String,
+                   let identifier = value?["identifier"] as? String {
+                    let room = ChatRoom(title: title, messages: [], identifier: identifier)
+                    rooms.append(room)
+                }
+            }
+            completion(rooms)
+        }
+    }
+    
+    func fetchMessages(chatRoom: ChatRoom, completion: @escaping ([Message]) -> Void) {
+        let chatId = chatRoom.identifier
+        var messages: [Message] = []
+        
+        ref.child(chatId).child("messages").observeSingleEvent(of: .value) { (snapshot) in
+            for child in snapshot.children {
+                let snap = child as? DataSnapshot
+                if let value = snap?.value as? NSDictionary {
+                    
+                    do {
+                        let decoder = JSONDecoder()
+                        decoder.dateDecodingStrategy = .millisecondsSince1970
+                        let data = try JSONSerialization.data(withJSONObject: value, options: [])
+                        let message = try decoder.decode(Message.self, from: data)
+                        messages.append(message)
+                    } catch {
+                        NSLog("Error decoding message: \(error)")
+                    }
+                }
+            }
+            completion(messages)
+        }
+    }
+    
+}
--- FirebaseChat/FirebaseChatTests/FirebaseChatTests.swift
+++ FirebaseChat/FirebaseChatTests/FirebaseChatTests.swift
@@ -6,28 +6,204 @@
 //
 
 import XCTest
+import Firebase
+import FirebaseDatabase
+
 @testable import FirebaseChat
 
 class FirebaseChatTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+    func testCreateChatRoom() {
+        let chatController = ChatController()
+        let title = "Test One"
+        chatController.createChatRoom(title: title)
+        XCTAssertEqual(chatController.chatRooms.count, 1)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testCreatedChatRoomWasAddedToFirebase() {
+        let chatController = ChatController()
+        let title = "Test Two"
+        var fetchedTitle = ""
+        let resultsExpectation = expectation(description: "Wait for results")
+        
+        chatController.createChatRoom(title: title)
+        let identifier = chatController.chatRooms[0].identifier
+        chatController.ref.child(identifier).observeSingleEvent(of: .value, with: { (snapshot) in
+            let value = snapshot.value as? NSDictionary
+            if let fetchTitle = value?["title"] as? String {
+                fetchedTitle = fetchTitle
+                resultsExpectation.fulfill()
+            }
+        })
+        wait(for: [resultsExpectation], timeout: 1)
+        XCTAssertEqual(fetchedTitle, title)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testDeleteChatRoom_ShouldRemoveFromServerAndLocalArray() {
+        let chatController = ChatController()
+        let title = "Test Deleting ChatRoom"
+        chatController.createChatRoom(title: title)
+        
+        XCTAssertEqual(chatController.chatRooms.count, 1)
+        
+        let resultsExpectation = expectation(description: "Wait for results")
+        
+        let chatRoom = chatController.chatRooms[0]
+        let identifier = chatRoom.identifier
+        var results: NSDictionary?
+        
+        chatController.deleteChatRoom(chatRoom: chatRoom)
+        
+        chatController.ref.child(identifier).observeSingleEvent(of: .value, with: { (snapshot) in
+            results = snapshot.value as? NSDictionary
+            resultsExpectation.fulfill()
+        })
+        
+        wait(for: [resultsExpectation], timeout: 1)
+        XCTAssertNil(results)
+        XCTAssertNil(chatController.chatRooms.firstIndex(of: chatRoom))
+        XCTAssertEqual(chatController.chatRooms.count, 0)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testCreateMessage_ShouldAddToServerAndLocalArray() {
+        let chatController = ChatController()
+        let title = "Test Three - with Message"
+        chatController.createChatRoom(title: title)
+        
+        let chatRoom = chatController.chatRooms[0]
+        let sender = Sender(displayName: "Cora")
+        let text = "Testing..."
+        
+        let resultsExpectation = expectation(description: "Wait for results")
+        let identifier = chatRoom.identifier
+        var resultText = ""
+        
+        chatController.createMessage(chatRoom: chatRoom, sender: sender, text: text)
+        
+        XCTAssertEqual(text, chatRoom.messages[0].text)
+        
+        let messageId = chatRoom.messages[0].messageId
+        
+        chatController.ref.child(identifier).child("messages").child(messageId).observeSingleEvent(of: .value, with: { (snapshot) in
+            if let value = snapshot.value as? NSDictionary {
+                resultText = value["text"] as? String ?? ""
+                resultsExpectation.fulfill()
+            }
+        })
+        
+        wait(for: [resultsExpectation], timeout: 1)
+        XCTAssertEqual(text, resultText)
+    }
+    
+    func testDeleteMessage_ShouldRemoveFromServerAndLocalArray() {
+        let chatController = ChatController()
+        let title = "Test Four - Deleted Message"
+        chatController.createChatRoom(title: title)
+        
+        let chatRoom = chatController.chatRooms[0]
+        let sender = Sender(displayName: "Cora")
+        let text = "Testing..."
+        
+        let setUpExpectation = expectation(description: "Wait for set up")
+        let identifier = chatRoom.identifier
+        var resultText = ""
+        
+        chatController.createMessage(chatRoom: chatRoom, sender: sender, text: text)
+        
+        XCTAssertEqual(chatRoom.messages.count, 1)
+        XCTAssertEqual(text, chatRoom.messages[0].text)
+        
+        let message = chatRoom.messages[0]
+        let messageId = message.messageId
+        
+        chatController.ref.child(identifier).child("messages").child(messageId).observeSingleEvent(of: .value, with: { (snapshot) in
+            if let value = snapshot.value as? NSDictionary {
+                resultText = value["text"] as? String ?? ""
+                setUpExpectation.fulfill()
+            }
+        })
+        
+        wait(for: [setUpExpectation], timeout: 1)
+        XCTAssertEqual(text, resultText)
+        
+        let resultsExpectation = expectation(description: "Wait for results")
+        var value: NSDictionary?
+        
+        chatController.deleteMessage(chatRoom: chatRoom, message: message)
+        
+        chatController.ref.child(identifier).child("messages").child(messageId).observeSingleEvent(of: .value, with: { (snapshot) in
+            value = snapshot.value as? NSDictionary
+            resultsExpectation.fulfill()
+        })
+        
+        wait(for: [resultsExpectation], timeout: 1)
+        XCTAssertEqual(chatRoom.messages.count, 0)
+        XCTAssertNil(value)
+    }
+    
+    func testFetchChatRooms_ShouldReturnChatRooms() {
+        let chatController = ChatController()
+        let resultsExpectation = expectation(description: "Wait for results")
+        
+        XCTAssertEqual(chatController.chatRooms.count, 0)
+        
+        chatController.fetchChatRooms { (rooms) in
+            chatController.chatRooms = rooms
+            resultsExpectation.fulfill()
         }
+        
+        wait(for: [resultsExpectation], timeout: 2)
+
+        XCTAssertNotEqual(chatController.chatRooms.count, 0)
+    }
+    
+    func testFetchMessages_ShouldReturnTestMessageText() {
+        let chatController = ChatController()
+        let resultsExpectation = expectation(description: "Wait for results")
+
+        chatController.fetchChatRooms { (rooms) in
+            chatController.chatRooms = rooms
+            resultsExpectation.fulfill()
+        }
+
+        let testRoomId = "E6DB4CB4-F5DE-417C-96C0-541235BE42FE"
+        let testMessage = "Testing..."
+
+        wait(for: [resultsExpectation], timeout: 2)
+        let testRoomIndex = chatController.chatRooms.firstIndex(where: { $0.identifier == testRoomId }) ?? 100
+        XCTAssertNotEqual(chatController.chatRooms.count, 0)
+        XCTAssertNotEqual(testRoomIndex, 100)
+
+        let messageExpectation = expectation(description: "Wait for messages")
+
+        chatController.fetchMessages(chatRoom: chatController.chatRooms[testRoomIndex]) { (messages) in
+            chatController.chatRooms[testRoomIndex].messages = messages
+            messageExpectation.fulfill()
+        }
+
+        wait(for: [messageExpectation], timeout: 2)
+        XCTAssertEqual(chatController.chatRooms[testRoomIndex].messages.count, 1)
+
+        let testRoomMessage = chatController.chatRooms[testRoomIndex].messages[0].text
+
+        XCTAssertEqual(testMessage, testRoomMessage)
+    }
+    
+    func testFetchEverything_ShouldReturnChatRoomsAndMessages() {
+        let chatController = ChatController()
+        let resultsExpectation = expectation(description: "Wait for results")
+        
+        chatController.fetchEverything {
+            resultsExpectation.fulfill()
+        }
+        
+        wait(for: [resultsExpectation], timeout: 2)
+        XCTAssertNotEqual(chatController.chatRooms.count, 0)
+        
+        let testRoomId = "E6DB4CB4-F5DE-417C-96C0-541235BE42FE"
+        let testRoomIndex = chatController.chatRooms.firstIndex(where: { $0.identifier == testRoomId }) ?? 100
+        XCTAssertNotEqual(testRoomIndex, 100)
+        XCTAssertEqual(chatController.chatRooms[testRoomIndex].messages.count, 1)
     }
 
 }
